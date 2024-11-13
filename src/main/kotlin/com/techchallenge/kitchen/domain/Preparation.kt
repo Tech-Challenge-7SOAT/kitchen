1543a7cb1d47cc60646878a802a79b71a43318d5
Rafael Acioly <aciolyr@gmail.com>
2024-11-13 19:44:27 -0300

    add docker file

--- src/main/kotlin/com/techchallenge/kitchen/domain/Preparation.kt
+++ src/main/kotlin/com/techchallenge/kitchen/domain/Preparation.kt
@@ -1,7 +1,39 @@
 package com.techchallenge.kitchen.domain
 
+import jakarta.persistence.*
+import org.hibernate.proxy.HibernateProxy
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+
+@Entity
+@EntityListeners(AuditingEntityListener::class)
+@Table(name = "preparations")
 data class Preparation(
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    var id: Long? = null,
     val orderId: String,
     val dueData: String,
     val status: String
-)
+) {
+    final override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null) return false
+        val oEffectiveClass =
+            if (other is HibernateProxy) other.hibernateLazyInitializer.persistentClass else other.javaClass
+        val thisEffectiveClass =
+            if (this is HibernateProxy) this.hibernateLazyInitializer.persistentClass else this.javaClass
+        if (thisEffectiveClass != oEffectiveClass) return false
+        other as Preparation
+
+        return id != null && id == other.id
+    }
+
+    final override fun hashCode(): Int =
+        if (this is HibernateProxy) this.hibernateLazyInitializer.persistentClass.hashCode() else javaClass.hashCode()
+
+    @Override
+    override fun toString(): String {
+        return this::class.simpleName + "(  orderId = $orderId )"
+    }
+}
