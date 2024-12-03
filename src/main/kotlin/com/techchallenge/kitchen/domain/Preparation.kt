b724b88a9552b1fc5be5f27578eb97690af015fc
Rafael Acioly <aciolyr@gmail.com>
2024-12-02 22:27:43 -0300

    remove unnecessary method from data class

--- src/main/kotlin/com/techchallenge/kitchen/domain/Preparation.kt
+++ src/main/kotlin/com/techchallenge/kitchen/domain/Preparation.kt
@@ -15,25 +15,4 @@ data class Preparation(
     val orderId: String,
     val dueDate: String,
     val status: String
-) {
-    final override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (other == null) return false
-        val oEffectiveClass =
-            if (other is HibernateProxy) other.hibernateLazyInitializer.persistentClass else other.javaClass
-        val thisEffectiveClass =
-            if (this is HibernateProxy) this.hibernateLazyInitializer.persistentClass else this.javaClass
-        if (thisEffectiveClass != oEffectiveClass) return false
-        other as Preparation
-
-        return id != null && id == other.id
-    }
-
-    final override fun hashCode(): Int =
-        if (this is HibernateProxy) this.hibernateLazyInitializer.persistentClass.hashCode() else javaClass.hashCode()
-
-    @Override
-    override fun toString(): String {
-        return this::class.simpleName + "(  orderId = $orderId )"
-    }
-}
+)
